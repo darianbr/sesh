01eb1452355f1e1c561ff739fa9289b76436a7fd
Darian Brown <darianbr@gmail.com>
2012-10-18 11:11:25 +1000

    added phpunit tests to most methods

--- test/Succinct/Sesh/Sesh_Test.php
+++ test/Succinct/Sesh/Sesh_Test.php
@@ -1,13 +1,49 @@
 <?php
 
-use Succinct\Dbal\Database;
+use Succinct\Sesh\Sesh;
 
 class Sesh_Test extends PHPUnit_Framework_TestCase {
 
-	public function testTFW() {
-		$db = new Sesh();
-		$this->assertEquals(1,1);
+	protected $sesh = NULL;
 
+	protected function setUp() {
+		parent::setUp();
+
+		$this->sesh = new Sesh();
 	}
 
+	/**
+     * @runInSeparateProcess
+     */
+	// TODO: Fix - trying to register session here conflicts with already started sesion by setUp() method
+	// public function testInit() {
+	// 	$sesh = NULL;
+	// 	$this->assertNull($sesh);
+	// 	$sesh = new Sesh();
+	// 	$this->assertEquals(get_resource_type($sesh), 'Sesh');
+	// }
+
+	/**
+     * @runInSeparateProcess
+     * @covers Sesh::set();
+     * @covers Sesh::__set();
+     */
+	public function testSet() {
+		$bool = $this->sesh->set('foo', 'bar');
+		$this->assertTrue($bool);
+	}
+
+	/**
+     * @runInSeparateProcess
+     * @covers Sesh::get();
+     * @covers Sesh::__get();
+     */
+	public function testGet() {
+		$value = $this->sesh->get('foo');
+		$this->assertNull($value);
+
+		$value = $this->sesh->set('foo', 'bar');
+		$value = $this->sesh->get('foo');
+		$this->assertSame($value, 'bar');
+	}
 }
