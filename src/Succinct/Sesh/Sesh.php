1049bf9b1d4f7e60063876f5976eb171533fa304
Darian Brown <darianbr@gmail.com>
2013-02-05 10:01:14 +1000

    Optimised and reordered some methods

--- src/Succinct/Sesh/Sesh.php
+++ src/Succinct/Sesh/Sesh.php
@@ -9,12 +9,14 @@ class Sesh {
 	
 	public function __construct() {
 
-		if (headers_sent()) {
-			throw new \RuntimeException('Failed to initialise session. Headers already sent');
-		}
+		if ('' === session_id()) {
+			if (headers_sent()) {
+				throw new \RuntimeException('Failed to initialise session. Headers already sent');
+			}
 
-		if (!session_start()) {
-			throw new \RuntimeException('Failed to initialise session.');
+			if (!session_start()) {
+				throw new \RuntimeException('Failed to initialise session.');
+			}
 		}
 
 		$this->data = array_key_exists(self::SESSION_CONTAINER, $_SESSION) ? $_SESSION[self::SESSION_CONTAINER] : array();
@@ -24,57 +26,6 @@ class Sesh {
 
 		$_SESSION[self::SESSION_CONTAINER] = $this->data;
 		$this->data = NULL;
-	}
-
-	/**
-	 * @param $option string|array
-	 * @return bool
-	 */
-	public function set($option, $value=array()) {
-
-		if ($option === (array) $option) {
-			foreach ($option as $key => $value) {
-				$this->data[$key] = $value;
-			}
-			return TRUE;
-		} else {
-			$this->data[$option] = $value;
-			return array_key_exists($option, $this->data);
-		}
-	}
-
-	/**
-	 * @param $option string|array
-	 * @return bool
-	 */
-	public function remove($option) {
-
-		if ($option === (array) $option) {
-			foreach ($option as $key => $value) {
-				$this->remove($key);
-			}
-			return TRUE;
-		} else if (array_key_exists($option, $this->data)) {
-			$this->data[$option] = NULL;
-			unset($this->data[$option]);
-			return !array_key_exists($option, $this->data);
-		} else {
-			return NULL;
-		}
-	}
-
-	public function destroy() {
-
-		session_destroy();
-	}
-
-	/**
-	 * @param $option string
-	 * @return mixed
-	 */
-	public function get($option) {
-
-		return array_key_exists($option, $this->data) ? $this->data[$option] : NULL;
 	}
 
 	/**
@@ -93,5 +44,68 @@ class Sesh {
 	public function __get($option) {
 
 		return $this->get($option);
+	}
+
+	public function __isset($option) {
+		return array_key_exists($option, $this->data);
+	}
+
+	public function __unset($option) {
+		return $this->remove($option);
+	}
+
+	public function id() {
+		return session_id();
+	}
+
+	/**
+	 * @param $option string|array
+	 * @return bool
+	 */
+	public function set($option, $value=array()) {
+
+		if ($option === (array) $option) {
+			foreach ($option as $key => $value) {
+				$this->set($key, $value);
+			}
+			return TRUE;
+		} else {
+			$this->data[$option] = $value;
+			return array_key_exists($option, $this->data);
+		}
+	}
+
+	/**
+	 * @param $option string
+	 * @return mixed
+	 */
+	public function get($option) {
+
+		return array_key_exists($option, $this->data) ? $this->data[$option] : NULL;
+	}
+
+	/**
+	 * @param $option string|array
+	 * @return bool
+	 */
+	public function remove($option) {
+
+		if ($option === (array) $option) {
+			foreach ($option as $key) {
+				$this->remove($key);
+			}
+			return TRUE;
+		} else if (array_key_exists($option, $this->data)) {
+			$this->data[$option] = NULL;
+			unset($this->data[$option]);
+			return !array_key_exists($option, $this->data);
+		} else {
+			return NULL;
+		}
+	}
+
+	public function destroy() {
+
+		session_destroy();
 	}
 }
